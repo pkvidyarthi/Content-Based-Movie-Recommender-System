86d016e7a7f4db6991d2a8f4cc1b4656d203fcba
Prince Kumar <72149077+pkvidyarthi@users.noreply.github.com>
2025-10-06 11:10:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,18 @@ import requests
 
 # Function for fetching poster
 def posterFetching(movie_id):
-    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=009859e96aae00ca3e03550fbdafd804&language=en-US'.format(movie_id))
+    response = requests.get(
+        f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=009859e96aae00ca3e03550fbdafd804&language=en-US'
+    )
     data = response.json()
-    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+    return "https://image.tmdb.org/t/p/w500/" + data.get('poster_path', '')
 
 # Function for recommending movies
 def Recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     movie_index = int(movie_index)
     distances = similarity[movie_index]
-    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x:x[1])[1:6]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
     recommendedMovies = []
     recommendPoster = []
     for i in movies_list:
@@ -23,20 +25,23 @@ def Recommend(movie):
         recommendPoster.append(posterFetching(movie_id))
     return recommendedMovies, recommendPoster
 
-# Load movie data from JSON file
-with open('movie.json', 'r') as f:
-    movies_dict = json.load(f)
-
+# ✅ Load movie data from GitHub (raw link)
+file_url = 'https://raw.githubusercontent.com/pkvidyarthi/Content-Based-Movie-Recommender-System/main/movie.json'
+response = requests.get(file_url)
+movies_dict = response.json()
 movies = pd.DataFrame(movies_dict)
 
-# Load JSON string from file
-file_url = 'https://github.com/pkvidyarthi/Content-Based-Movie-Recommender-System/raw/main/similarity.json'
-similarity = pd.read_json(file_url)
+# ✅ Load similarity data from GitHub (raw link)
+similarity_url = 'https://raw.githubusercontent.com/pkvidyarthi/Content-Based-Movie-Recommender-System/main/similarity.json'
+similarity = pd.read_json(similarity_url)
 
-st.title('Movie Recommender System')
+st.title('🎬 Movie Recommender System')
 
 # Selectbox from streamlit
-option = st.selectbox('Please select your favorite movie, as my job is to recommend some movies to you', movies['title'].values)
+option = st.selectbox(
+    'Please select your favorite movie, as my job is to recommend some movies to you',
+    movies['title'].values
+)
 
 # Button
 if st.button('Recommend'):
